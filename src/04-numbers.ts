4ae5002c96a08d1f11106afde56e7d44078f3768
Pablo <pablogarcirodriguez@gmail.com>
2023-01-11 17:32:32 +0100

    Clase 9: numbers

---
+++ src/04-numbers.ts
@@ -0,0 +1,32 @@
+(() => {
+  let productPrice = 100;
+  productPrice = 12;
+  console.log('productPrice', productPrice)
+
+  let customerAge: number = 28;
+  customerAge = customerAge + 1;
+  console.log('customerAge', customerAge)
+
+  let productInStock: number;
+  console.log('productInStock', productInStock);
+
+  if(productInStock>10){
+    console.log('yes');
+  }
+
+  let discount = parseInt('100');
+  console.log('discount', discount);
+
+  if (discount<=200) {
+    console.log('aplicar');
+  }else{
+    console.log('no aplicar');
+  }
+
+  let hex = 0xfff;
+  console.log('hex', hex)
+  let bin = 0b010101;
+  console.log('bin', bin);
+
+
+})();
