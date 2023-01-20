22778c467d6ec06edaf6b368cf72e6059dba39bc
Pablo <pablogarcirodriguez@gmail.com>
2023-01-20 17:42:05 +0100

    Clase 18: Retorno de funciones

--- src/13-return-functions.ts
+++ src/13-return-functions.ts
@@ -17,3 +17,42 @@
 
   printTotal([1, 2, 3, 4, 5])
 })();
+
+// En TypeScript podemos especificar el tipo de dato del valor que nos retornará una función o indicar si no se devolverá dato alguno:
+
+// Retornos tipados en TypeScript
+// El tipo de retorno se especificará después de los paréntesis en los que se encuentran los argumentos de la función:
+
+// Void: funciones sin retorno
+// Este tipo de función ejecuta ciertas instrucciones, pero no devuelve dato alguno. Estas son conocidas como funciones de tipo void. Se definen así:
+// //TypeScript
+// function imprimirNombre(yourName: string): void {
+//     console.log(`Hello ${yourName}`);
+// }
+// Funciones con retorno
+// Por el contrario, si en la función devolveremos algún valor, podemos especificar el tipo de dato de este:
+// //TypeScript
+// function suma(a: number, b: number): number {
+//     return a + b;
+// }
+
+// function holaMundo(): string {
+//     return "Hello, World!";
+// }
+// También los retornos pueden ser más de un tipo de dato:
+
+// //TypeScript
+// function devolverMayor(a: number, b: number): number | string {
+//     if(a > b){
+//         // Retorna un número
+//         return a;
+//     } else if( b > a ) {
+//         // Retorna un número
+//         return b;
+//     } else {
+//         // Retorna un string
+//         return `Los números ${a} y ${b} son iguales`;
+//     }
+// }
+// TypeScript también lo infiere
+// Si no indicamos en nuestra declaración de la función el tipado del retorno, TypeScript, al igual que con las variables, lo puede inferir según si retornas datos (sea string, number, etc.) o si nada es devuelto (tipo void).
